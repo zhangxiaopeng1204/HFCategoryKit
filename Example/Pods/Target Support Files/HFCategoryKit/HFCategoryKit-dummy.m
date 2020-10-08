66f7fff449499eb66247c5236715a61db72848bf
张敬朋 <510631648@qq.com>
2020-10-08 20:00:24 +0800

    添加Person类

---
+++ Example/Pods/Target Support Files/HFCategoryKit/HFCategoryKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HFCategoryKit : NSObject
+@end
+@implementation PodsDummy_HFCategoryKit
+@end
---
+++ HFCategoryKit/Classes/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  ZJHDemo
+//
+//  Created by huifan on 2020/10/8.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name = "zhangsan"
+    var age = 10
+    
+}
